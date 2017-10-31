48fb2fc8b48c390b96898a042c427948606c5a18
liebeskind <daniel.liebeskind@gmail.com>
2017-10-31 14:45:59 +0700

    Initializes repo

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactNative from 'react-native';
+import {Text} from 'react-native';
+
+export const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
