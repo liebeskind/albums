b6569f57fbc1aa9803b7c8f2cafa62e67035c925
liebeskind <daniel.liebeskind@gmail.com>
2017-11-02 20:14:14 +0800

    Adds a header component

--- ios.index.js
+++ ios.index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Header from './src/components/header';
 
 const App = () => (
-  <Text>Some Text</Text>
+  <Header />
 );
 
 export default App;
