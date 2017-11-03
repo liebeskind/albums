aae76d70547a4890cf783a36bb19cad2bc29b5c6
liebeskind <daniel.liebeskind@gmail.com>
2017-11-03 10:57:12 +0800

    Adds axios and basic http request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = {};
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
+  }
+
 	render() {
     return (
       <View>
-        <Text>Album List!!!</Text>
+        <Text>Album List!!</Text>
       </View>
     );
   }
