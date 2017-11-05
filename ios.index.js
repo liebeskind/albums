8c68e8d347e42ecc089e9df00bf1cc2f71c85260
liebeskind <daniel.liebeskind@gmail.com>
2017-11-05 15:13:18 +0800

    Adds flex:1 to root component to make it fill entire contents (necessary for correct scrolling).

--- ios.index.js
+++ ios.index.js
@@ -3,8 +3,9 @@ import { View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 
+//flex:1 makes it so component fills the entire content area of the device for scrolling.
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
